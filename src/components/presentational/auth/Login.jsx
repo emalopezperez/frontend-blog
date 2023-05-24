94dc323ad99dc0dfbef5ec6b0402181f3f6f7724
emanuel lopez <emanuel-lopez13@hotmail.com>
2023-05-24 16:42:12 -0300

    Agregue formularios de login y de registro

--- src/components/presentational/auth/Login.jsx
+++ src/components/presentational/auth/Login.jsx
@@ -1,9 +1,29 @@
-
+import "./form.css";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="form-container">
+      <p className="title">Login</p>
+      <form className="form">
+        <div className="input-group">
+          <label>Username</label>
+          <input type="text" name="username" id="username" placeholder="" />
+        </div>
+        <div className="input-group">
+          <label>Password</label>
+          <input type="password" name="password" id="password" placeholder="" />
+        </div>
+        <input type="submit" className="sign" value="Login"/>
+      </form>
+      
+      <p className="signup">
+        Dont have an account?
+        <a rel="noopener noreferrer" href="#" className="">
+          Sign up
+        </a>
+      </p>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/components/presentational/auth/Registrarse.jsx
+++ src/components/presentational/auth/Registrarse.jsx
@@ -1,11 +1,33 @@
-
+import "./form.css";
 
 const Registrarse = () => {
   return (
-    <div>
-    registro
-    </div>
-  )
-}
+    <div className="form-container">
+      <p className="title">Registrarse</p>
+      <form className="form">
+        <div className="input-group">
+          <label>Username</label>
+          <input type="text" name="username" id="username" placeholder="" />
+        </div>
+        <div className="input-group">
+          <label>Email</label>
+          <input type="text" name="email" id="email" placeholder="" />
+        </div>
+        <div className="input-group">
+          <label>Password</label>
+          <input type="password" name="password" id="password" placeholder="" />
+        </div>
+        <input type="submit" className="sign" value="Registrarse" />
+      </form>
 
-export default Registrarse
+      <p className="signup">
+        Dont have an account?
+        <a rel="noopener noreferrer" href="#" className="">
+          Login
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Registrarse;
